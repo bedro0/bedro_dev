545b062203e2f35e6f6aae317625fe1ab8d7913a
bedro <badri@loonartech.net>
2026-05-21 02:01:01 -0400

    Added logos and fixed styles.

--- src/components/Bio.tsx
+++
@@ -1,7 +0,0 @@
-export default function Bio() {
-    return (<div className="text-2xl">
-        <p>
-            Hello! My name is Badri and I like to build software.
-        </p>
-    </div>)
-}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,11 @@
 export default function NavBar() {
     return (
-        <nav className="text-green-100 my-8 mx-8 text-3xl flex justify-between">
-            <a href="/">bedro.dev</a>
+        <nav className="text-green-100 my-8 mx-8 sm:text-3xl text-lg flex justify-between">
             <div>
+                <a className="sm:mx-8 mx-1" href="/">bedro.dev</a>
+                <a className="sm:mx-8 mx-1" href="#projects">Projects</a>
+            </div>
+            <div className="flex items-center">
                 <Socials />
             </div>
         </nav>
@@ -13,25 +16,32 @@ function Socials() {
     const socials = [
         {
             platform: "GitHub",
-            url: "https://github.com/bedro0"
+            url: "https://github.com/bedro0",
+            logo: "GitHub_Invertocat_White.svg"
         },
         {
             platform: "LinkedIn",
-            url: "https://www.linkedin.com/in/bedro0/"
+            url: "https://www.linkedin.com/in/bedro0/",
+            logo: "InBug-White.png"
         },
         {
             platform: "Twitch",
-            url: "https://www.twitch.tv/bedro_dev"
+            url: "https://www.twitch.tv/bedro_dev",
+            logo: "glitch_flat_white.svg"
         }
     ]
     return (
-        <div>
-            {socials.map(({ platform, url }) => (
+        <div className="flex">
+            {socials.map(({ platform, url, logo }) => (
                 <a
-                    className="mx-2"
+                    className="mx-2 hover:cursor-pointer"
                     key={platform}
                     href={url}>
-                    {platform}
+                    <img
+                        className="w-8 mx-0.5"
+                        src={`/src/assets/${logo}`}
+                        alt={platform}
+                    />
                 </a>
             ))}
         </div>
