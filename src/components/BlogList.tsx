9b9159ad4ba5aa016c8fcba1595b04c3166ad5e5
bedro <badri@loonartech.net>
2026-07-22 00:25:04 -0400

    Added Experience

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,7 +1,7 @@
 import type { Post } from "@/lib/blogPosts"
 
 export default function BlogList({ posts }: { posts: Post[] }) {
-    return <div>
+    return <div className="flex flex-col gap-8 mx-8 sm:mx-16">
         {
             posts.map((post, index) => <DisplayPost key={index} post={post} />
             )
@@ -11,15 +11,15 @@ export default function BlogList({ posts }: { posts: Post[] }) {
 
 function DisplayPost({ post: { url, frontmatter: { title, description, author, updated_date, tags } } }: { post: Post }) {
     return <a href={url}>
-        <div className="bg-green-900 mx-16 my-4 p-8 rounded-2xl">
-            <p>{updated_date}</p>
+        <div className="bg-primary p-8 rounded-2xl">
             <h1 className="text-2xl">{title}</h1>
+            <p>{updated_date}</p>
             <div className="flex">
-                <p className="bg-green-950 p-2 mt-4 rounded-lg">{description}</p>
+                <p className=" py-2 mt-4 rounded-lg">{description}</p>
             </div>
-            <div className="flex mt-4 gap-2 rounded">
+            <div className="flex flex-wrap mt-4 gap-2 rounded">
                 {tags.map((tag, index) => (
-                    <div key={index} className="bg-green-950 px-2 rounded">{tag}</div>
+                    <div key={index} className="px-2 bg-secondary rounded">{tag}</div>
                 ))}
             </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,43 @@ import logoLinkedIn from "@/assets/linkedin.svg"
 import logoTwitch from "@/assets/glitch_flat_white.svg"
 import logoEmail from "@/assets/envelope.svg"
 import logoLocation from "@/assets/location.svg"
+import logoHamburger from "@/assets/hamburger.svg"
 import { useEffect, useState } from "react"
-// Todo add location
 export default function Header() {
+    const [hamburgerMenuEnabled, setHamburgerMenuEnabled] = useState(false)
     return (
-        <nav className="text-green-100 my-8 mx-8 sm:text-3xl text-lg flex justify-between whitespace-nowrap">
-            <div>
-                <a className="sm:mx-8 mx-1" href="/">bedro.dev</a>
-                <a className="sm:mx-8 mx-1 hidden sm:inline" href="/blog">Blog</a>
+        <nav className="text-foreground my-8 mx-8">
+            <div className="flex justify-between whitespace-nowrap">
+                <div className="flex">
+                    <a className="text-3xl" href="/">bedro.dev</a>
+                    <div className="flex items-center">
+                        <Location />
+                        <div className="hidden md:inline"><Socials /></div>
+                    </div>
+                </div>
+                <div className="hidden lg:inline text-3xl">
+                    <a className="sm:mx-8 mx-1" href="/blog">Blog</a>
+                    <a className="sm:mx-8 mx-1" href="/about">About</a>
+                </div>
+                <div className="lg:hidden inline shrink-0">
+                    <button
+                        onClick={() => {
+                            setHamburgerMenuEnabled(!hamburgerMenuEnabled)
+                        }}
+                    >
+                        <img className="h-8 mx-0.5 hover:cursor-pointer" alt="Expand" src={logoHamburger.src} />
+                    </button>
+                </div>
             </div>
-            <div className="flex items-center">
-                <Location />
-                <Socials />
+            <div className="flex justify-between">
+                <div className="inline md:hidden my-6"><Socials /></div>
+                <div></div>
+                {hamburgerMenuEnabled && <div>
+                    <div className="lg:hidden flex flex-col items-end gap-2 text-xl">
+                        <a href="/blog">Blog</a>
+                        <a href="/about">About</a>
+                    </div>
+                </div>}
             </div>
         </nav>
     )
@@ -48,7 +73,7 @@ function Socials() {
         <div className="flex items-center justify-center">
             {socials.map(({ platform, url, logo }) => (
                 <a
-                    className="mx-2 hover:cursor-pointer min-w-8"
+                    className="mr-4 hover:cursor-pointer shrink-0"
                     key={platform}
                     href={url}>
                     <img
@@ -86,13 +111,13 @@ function Location() {
     })
 
     return <div className="relative text-white">
-        <div className="peer flex justify-center items-center px-8 cursor-default min-w-8">
+        <div className="peer flex justify-center items-center px-8 cursor-default min-w-8 lg:text-3xl text-lg">
             <img className="h-8 mr-2"
                 src={logoLocation.src}
                 alt="Location"
             />
             <p className="font-bold">{city}</p>
         </div>
-        <p className="text-xl opacity-0 peer-hover:opacity-100 invisible peer-hover:visible transition-opacity duration-100 ease-in-out absolute bg-green-900 rounded-lg top-full left-1/2 -translate-x-1/2 translate-y-1/4 p-2">{time}</p>
+        <p className="text-xl opacity-0 peer-hover:opacity-100 invisible peer-hover:visible transition-opacity duration-100 ease-in-out absolute bg-primary rounded-lg top-full left-1/2 -translate-x-1/2 translate-y-1/4 p-2">{time}</p>
     </div>
 }
