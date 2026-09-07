edba90ed4d6f5540950e7b6c2e18416f5655bd65
bedro <badri@loonartech.net>
2026-09-06 22:42:05 -0400

    Fixed cards to match visual border to actual clickable area.

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -10,25 +10,23 @@ export type ExperienceCardProps = {
 };
 
 export default function ExperienceCard({ href, title, subtitle, dateLabel, headline, skills }: ExperienceCardProps) {
-    return <div className="border border-border rounded-lg hover:border-primary px-8 w-full sm:w-sm">
-        <a className="flex flex-col gap-1"
-            href={href}
-        >
-            <div className="border-b border-border pt-8 pb-4 flex flex-col">
-                <div>
-                    <h4 className="text-2xl ">{title}</h4>
-                    {subtitle && <h4 className="text-md">{subtitle}</h4>}
-                </div>
-                <h5 className="whitespace-nowrap text-sm">{dateLabel}</h5>
+    return <a className="flex flex-col gap-1 border border-border rounded-lg hover:border-primary px-8 w-full sm:w-sm"
+        href={href}
+    >
+        <div className="border-b border-border pt-8 pb-4 flex flex-col">
+            <div>
+                <h4 className="text-2xl ">{title}</h4>
+                {subtitle && <h4 className="text-md">{subtitle}</h4>}
             </div>
-            <div className="flex flex-col gap-4 pt-4 pb-8 justify-end" >
-                <p className="">{headline}</p>
-                <div className="flex flex-wrap gap-2">
-                    {skills.slice(0, 3).map((skill) => (
-                        <p key={skill} className="border border-border rounded px-2 bg-accent">{skill}</p>
-                    ))}
-                </div>
+            <h5 className="whitespace-nowrap text-sm">{dateLabel}</h5>
+        </div>
+        <div className="flex flex-col gap-4 pt-4 pb-8 justify-end" >
+            <p className="">{headline}</p>
+            <div className="flex flex-wrap gap-2">
+                {skills.slice(0, 3).map((skill) => (
+                    <p key={skill} className="border border-border rounded px-2 bg-accent">{skill}</p>
+                ))}
             </div>
-        </a>
-    </div>
+        </div>
+    </a>
 }
