39d143be877d40651347715e9e94353786a71349
bedro <bedro@loonartech.net>
2026-09-14 00:48:54 -0400

    Added "k-Window Recirculating Queue" blog post.
    Updated npm packages

--- src/components/DisplayBlog.tsx
+++ src/components/DisplayBlog.tsx
@@ -2,7 +2,7 @@ import { blogPosts, skillsForPost } from "@/lib/blogPosts";
 import BlogTeaserCard from "@/components/BlogTeaserCard";
 
 export default function DisplayBlog() {
-    const latestPosts = blogPosts.slice(0, 2);
+    const latestPosts = blogPosts.slice(0, 3);
     return <div>
         <a href="/blog"><h3 className="text-4xl hover:text-sidebar-primary w-fit">Blog</h3></a>
         <div className="flex flex-col sm:flex-row flex-wrap gap-4 my-8">
